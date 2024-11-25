54a129e0715eb687e4eef1901bc12540343b6847
sbtik <aleksandrekekoshvili42@gmail.com>
2024-11-26 01:20:34 +0400

    commit

---
+++ level16/homeworke/hw2.py
@@ -0,0 +1,14 @@
+# მომხმარებელს შემოატანინეთ ასაკი,
+# გაარკვიეთ მას შეუძლია თუ არა მართვის მოწმობის აღება 
+# (17 წლის უნდა იყო მართვის მოწმობის აღება რომ შეგეძლოს).
+# თუ შეუძლია უთხარით რომ ის დიდი ბიჭი ან გოგოა, თუ არა დაუპრინტეთ რომ მალე გაიზრდება.
+
+age = int(input("enter_ur_age"))
+
+if age == age > 17:
+    print("she shegizlia aigo martvis mowmobis ageba")
+else:
+    print("shen ar shegizlia aigo martvis mowmoba")
+
+
+
